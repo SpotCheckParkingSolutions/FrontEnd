0da26c2d29bbca8e6d75ac6c450969d1321b2339
cooper-runstein <cooperun@gmail.com>
2018-08-04 17:17:27 -0700

    Client reponse with status error handled

--- src/Components/OnOffClientOption.js
+++ src/Components/OnOffClientOption.js
@@ -4,12 +4,22 @@ const OnOffClientOption = (props) =>{
   return (
     <div className="client-option">
       <span className="client-option-status">{props.optionName.toUpperCase()}:
-      {props.optionStatus === 0 ? 'OFF' : 'ON'}</span>
+      {props.optionStatus === 0 ? 'OFF' : (props.optionStatus === 1 ? 'ON' : 'ERROR')}</span>
+      {props.optionStatus !== 2
+      ?
       <button onClick={()=> {
         props.optionStatus === 0 ?
         alert(`this would turn ${props.optionName} on`) :
         alert(`this would turn ${props.optionName} off`)
-          }}> Turn {props.buttonName} {props.optionStatus === 0 ? 'On' : 'Off'}</button>
+          }}> Turn {props.buttonName} {props.optionStatus === 0 ? 'On' : 'Off'}
+      </button>
+      :
+      <button
+        style={{color: 'red'}}
+        onClick={()=>alert("This would offer error handling options")}
+      >Error</button>
+      }
+
     </div>
   )
 }
